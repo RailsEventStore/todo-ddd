e022527ebf323f8ca671331e2d96442a721135b4
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2022-12-13 22:50:20 +0100

    adding and displaying tasks + some styling

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,18 @@
 class TasksController < ApplicationController
   def index
     @tasks = Task.all
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(params.require(:task).permit(:description))
+
+    respond_to do |format|
+      if @task.save
+        format.html { redirect_to tasks_url, notice: "Task was successfully created" }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
   end
 end
